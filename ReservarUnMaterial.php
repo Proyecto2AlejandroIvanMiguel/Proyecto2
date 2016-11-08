df884085839ec305a4b8ea13e8e38c4c9838ecff
ARodriguez97 <4800.joan23@fje.edu>
2016-11-08 16:33:26 +0100

    Puesto con ID Usuario!

--- ReservarUnMaterial.php
+++ ReservarUnMaterial.php
@@ -9,11 +9,19 @@
 					echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
 					exit;
 			}
+			$usuario = $_POST['usuario'];
+			$usuarios = "SELECT * FROM tbl_usuario WHERE nombre_usuario = $usuario";
+			$sql = mysqli_query($conexion, $sql);
+			if(mysqli_num_rows($usuarios>0){
+				echo "Número de reservas: " . mysqli_num_rows($usuarios) . "<br/><br/>";
+			while($usuario = mysqli_fetch_array($usuarios)){
+				$id_usuario = $usuario['id_usuario']
+			}
 			$sin_nada = 0 ;
 			extract($_REQUEST);
       $hoy = date("Y-m-d H:i:s");
       $material=$_POST['materiales'];
-      $sql = "INSERT INTO bd_educayaprende.tbl_reserva (fecha_reserva, fechaF_reserva, estado_reserva) VALUES ($hoy , NULL, '0');"
+      $sql = "INSERT INTO bd_educayaprende.tbl_reserva (fecha_reserva, fechaF_reserva, estado_reserva , id_usuario) VALUES ($hoy ,'0000-00-00 00:00:00', '0' , $id_usuario);"
       $sql= mysqli_query($conexion, $sql);
       echo "Se ha efectuado su reserva";
     mysqli_close($conexion);
